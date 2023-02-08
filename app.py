f898aa920aff150e4bfe56bfe99eeba5027f8e48
Shruti Mundargi <48567754+shrutimundargi@users.noreply.github.com>
2023-02-08 00:23:49 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 import pandas as pd
 import numpy as np
-data = pd.read_csv("indian_food.csv")
-data = data.drop(columns=['flavor_profile','region','state', 'diet'])
-str_cols = list(data.columns)
-str_cols.remove('prep_time')
-str_cols.remove('cook_time')
-for i in str_cols:
-    data[i] = data[i].str.strip()
-    data[i] = data[i].str.lower()
-for i in ['name']:
-    data[i] = data[i].str.replace(" ", "-")
-for i in ['course']:
-    data[i] = data[i].str.replace(" ", "-")
-for i in ['ingredients']:
-    data[i] = data[i].str.replace(" ", "-")
-    data[i] = data[i].str.replace(",-", "_")
-    data[i] = data[i].str.replace("_", ", ")
-data['ingredients'] = list(data['ingredients'])
-def easyA(x):
-  available = []
-  suggested = []
-  #recommendation = np.empty((0, 3))
-  for i in range(0, len(data)):
-    count = 0
-    for j in x:
-        if j in data['ingredients'][i]:
-            count +=1
-            available.append(j)
-        if count >= 3:
-            series = pd.DataFrame({'recipe': [data['name'][i]], 'ingredients': data['ingredients'][i], 'score': "{:.2f}".format((len(available))/(len(data['ingredients'][i]))*100)})
-            recommendation = [data['name'][i],data['ingredients'][i],"{:.2f}".format((len(available))/(len(data['ingredients'][i]))*100)]
-            available.clear()
-    print(recommendation)
-    return recommendation
-   
-
-
 
 
 app = Flask(__name__)
@@ -46,11 +10,41 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         ingredients = request.form.get('fname')
-        res = easyA(ingredients)
-        return render_template("dishrec.html", res=res)
+        data = pd.read_csv("indian_food.csv")
+        data = data.drop(columns=['flavor_profile','region','state', 'diet'])
+        str_cols = list(data.columns)
+        str_cols.remove('prep_time')
+        str_cols.remove('cook_time')
+        for i in str_cols:
+            data[i] = data[i].str.strip()
+            data[i] = data[i].str.lower()
+        for i in ['name']:
+          data[i] = data[i].str.replace(" ", "-")
+        for i in ['course']:
+          data[i] = data[i].str.replace(" ", "-")
+        for i in ['ingredients']:
+          data[i] = data[i].str.replace(" ", "-")
+          data[i] = data[i].str.replace(",-", "_")
+          data[i] = data[i].str.replace("_", ", ")
+        data['ingredients'] = list(data['ingredients'])
+        available = []
+        suggested = []
+        recommendation = pd.DataFrame(columns = ['recipe', 'ingredients', 'score'])
+        for i in range(0, len(data)):
+            count = 0
+            for j in ingredients:
+                if j in data['ingredients'][i]:
+                    count +=1
+                    available.append(j)
+            if count >= 3:
+                series = pd.DataFrame({'recipe': [data['name'][i]], 'ingredients': data['ingredients'][i], 'score': "{:.2f}".format((len(available))/(len(data['ingredients'][i]))*100)})
+                recommendation = recommendation.append(series)
+                available.clear()
+                
+        return render_template("dishrec.html", tables=[recommendation.to_html(classes='data')],titles = ['Recommendations'])
 
     else: 
-        return render_template("dishrec.html")
+        return render_template("dishrec.html",res="")
 
 if __name__ == '__main__':
     app.run(debug = True)
